236d62f0e7a8ea127c5bf3906c3266d33f3da36f
Richard Szalay <richard@richardszalay.com>
2026-02-25 17:27:05 +1100

    Add WaitForFinalStatesAsync

--- RichardSzalay.MockHttp.WebSockets.Tests/MockWebSocketServerTests.cs
+++ RichardSzalay.MockHttp.WebSockets.Tests/MockWebSocketServerTests.cs
@@ -23,4 +23,37 @@ public class MockWebSocketServerTests
         
         Assert.Equal(HttpStatusCode.UpgradeRequired, response.StatusCode);
     }
+
+    [Fact]
+    public async Task WaitForFinalStateAsync_returns_after_sockets_are_all_closed_or_aborted()
+    {
+        var server = MockWebSocketServer.ForEndpoint(async (WebSocket ws, CancellationToken ct) =>
+        {
+            if (ws.State != WebSocketState.Aborted)
+            {
+                while (!ws.CloseStatus.HasValue)
+                {
+                    await ws.ReceiveAsync(Array.Empty<byte>(), ct);
+                }
+
+                await ws.CloseAsync(ws.CloseStatus.Value, ws.CloseStatusDescription, ct);
+            }
+        });
+
+        var client1 = new ClientWebSocket();
+        var client2 = new ClientWebSocket();
+        
+        await client1.ConnectAsync(new Uri("ws://localhost/"), server.ToMessageInvoker(), cancellationToken);
+        await client2.ConnectAsync(new Uri("ws://localhost/"), server.ToMessageInvoker(), cancellationToken);
+
+        await client1.CloseAsync(WebSocketCloseStatus.NormalClosure, null, cancellationToken);
+        client2.Abort();
+        
+        var finalStates = await server.WaitForFinalStatesAsync(cancellationToken);
+        
+        Assert.Equal([WebSocketState.Closed, WebSocketState.Aborted], finalStates.Order());
+        
+        Assert.Equal(WebSocketState.Closed, client1.State);
+        Assert.Equal(WebSocketState.Aborted, client2.State);
+    }
 }
